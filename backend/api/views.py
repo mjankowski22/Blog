3cea5e58fb5dc4105308a65fb7ad121507f25314
Michal Jankowski <rivivias@gmail.com>
2023-10-12 19:36:25 +0200

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from blog.models import Post
+from rest_framework import generics
+from .serializer import PostSerializer
+
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
---
+++ backend/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+
+def upload_location(instance,filename):
+    return 'posts/{0}/{1}'.format(instance.id,filename)
+
+class Post(models.Model):
+
+    class Meta:
+        ordering = ['-date']
+
+    title = models.CharField(max_length=100,unique=True)
+    content = models.TextField(max_length=1000)
+    slug = models.SlugField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True,blank=True,upload_to=upload_location)
+
+
+    def __str__(self):
+        return self.title
+
+    
+    
+
